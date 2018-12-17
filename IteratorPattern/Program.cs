5b1b1bf66fb1f38fa67634b6e422cd7beff6b9c4
Emirhan IŞIK <emir94han@hotmail.com>
2018-12-18 01:52:15 +0300

    Tasarım Kalıpları

--- IteratorPattern/Program.cs
+++ IteratorPattern/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Ödev3
+namespace Iterator
 {
-
     class Program
     {
         static void Main(string[] args)
